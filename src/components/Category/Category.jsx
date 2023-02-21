d29c81761efd7aaa3238736f4590c8924a74afd6
Pham Van Nhat <114339708+minhnhatzc@users.noreply.github.com>
2023-02-22 01:55:19 +0700

    update

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,5 +1,8 @@
 import "./Category.scss";
 import cat1 from '../../assets/category/cat-1.jpg'
+import cat2 from '../../assets/category/cat-2.jpg'
+import cat3 from '../../assets/category/cat-3.jpg'
+import cat4 from '../../assets/category/cat-4.jpg'
 const Category = () => {
   return (
     <div className="shop-by-category">
@@ -8,13 +11,13 @@ const Category = () => {
             <img src={cat1} alt=""/>
         </div>
         <div className="category">
-            <img src={cat1} alt=""/>
+            <img src={cat2} alt=""/>
         </div>
         <div className="category">
-            <img src={cat1} alt=""/>
+            <img src={cat3} alt=""/>
         </div>
         <div className="category">
-            <img src={cat1} alt=""/>
+            <img src={cat4} alt=""/>
         </div>
       </div>
     </div>
